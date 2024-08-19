fff07965ce85084e1030ec4fa371ef89780d1005
Brian Murithi <brianmurithi65@gmail.com>
2024-08-19 12:21:26 +0300

    Configure docker

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ gem "puma", "~> 6.0"
 # Bundle and transpile JavaScript [https://github.com/rails/jsbundling-rails]
 gem "jsbundling-rails"
 
+gem 'aws-sdk-s3'
+
+
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+   # Store uploaded files on the local file system (see config/storage.yml for options).
+   config.active_storage.service = :default
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
