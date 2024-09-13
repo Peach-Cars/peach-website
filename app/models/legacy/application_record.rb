84296f680c2cdf17a3dcc9f03f804e38b4ba95bd
Brian Murithi <brianmurithi65@gmail.com>
2024-09-13 15:18:25 +0300

    Disable database tasks from legacy db

--- app/models/legacy/application_record.rb
+++ app/models/legacy/application_record.rb
@@ -2,6 +2,6 @@
 module Legacy
   class ApplicationRecord < ActiveRecord::Base
     self.abstract_class = true
-    connects_to database: { writing: :legacy, reading: :legacy }
+    connects_to database: { reading: :legacy } 
   end
 end
