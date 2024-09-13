13a777e28c9599049a4942bd1638500fbcc5065f
Brian Murithi <brianmurithi65@gmail.com>
2024-09-13 15:54:47 +0300

    Annotate models

--- app/models/buyer_lead.rb
+++ app/models/buyer_lead.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: leads
+#
+#  id                  :bigint           not null, primary key
+#  full_name           :string
+#  email               :string
+#  phone               :string
+#  type                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  registration_number :string
+#  model_id            :integer
+#  year_of_manufacture :integer
+#  asking_price        :decimal(, )
+#  mileage             :integer
+#  location            :string
+#  listing_id          :integer
+#  requested_financing :boolean
+#  lead_source         :string
+#  consent             :boolean          default(FALSE), not null
+#  sent_to_freshsales  :boolean
+#  sent_to_django_api  :boolean
+#
 # Buyer lead model
 class BuyerLead < Lead
   validates :listing_id, :requested_financing, :lead_source, presence: true
--- app/models/seller_lead.rb
+++ app/models/seller_lead.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: leads
+#
+#  id                  :bigint           not null, primary key
+#  full_name           :string
+#  email               :string
+#  phone               :string
+#  type                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  registration_number :string
+#  model_id            :integer
+#  year_of_manufacture :integer
+#  asking_price        :decimal(, )
+#  mileage             :integer
+#  location            :string
+#  listing_id          :integer
+#  requested_financing :boolean
+#  lead_source         :string
+#  consent             :boolean          default(FALSE), not null
+#  sent_to_freshsales  :boolean
+#  sent_to_django_api  :boolean
+#
 # Seller leads model
 class SellerLead < Lead
   validates :registration_number, :model_id, :year_of_manufacture, :asking_price, :mileage, :consent, :location, presence: true
