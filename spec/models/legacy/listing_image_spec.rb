0032f2b56efbf332a37f8719cc04daa5b84dcd43
Brian Murithi <brianmurithi65@gmail.com>
2024-09-16 10:59:16 +0300

    Update readme and db config

--- spec/models/legacy/listing_image_spec.rb
+++ spec/models/legacy/listing_image_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ListingLocation, type: :model do
+RSpec.describe Legacy::ListingImage, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/legacy/listing_spec.rb
+++ spec/models/legacy/listing_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ListingTag, type: :model do
+RSpec.describe Legacy::Listing, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
