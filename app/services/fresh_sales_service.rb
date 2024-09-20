4b2e056a6856b09d288c0af7f6e28d9a550162d9
Brian Murithi <brianmurithi65@gmail.com>
2024-09-20 09:30:36 +0300

    Add concern to handle api errors

--- app/services/fresh_sales_service.rb
+++ app/services/fresh_sales_service.rb
@@ -1,22 +1,21 @@
 # Service to push contacts and vehicles to Freshsales
 class FreshSalesService < BaseApiService
+  include ApiErrorHandler
   option :base_url, default: -> { Rails.application.credentials.dig(:freshsales, :base_url) }
   option :token, default: -> { Rails.application.credentials.dig(:freshsales, :api_key) }
   option :auth_type, default: -> { 'Token' }
 
   def create_contact(data)
-    response = post('/crm/sales/api/contacts', data)
-    response.success?
-  rescue ApiError => e
-    Rails.logger.error "Failed to create contact in Freshsales: #{e.message}"
-    false
+    handle_api_errors do
+      response = post('/crm/sales/api/contacts', data)
+      response.success?
+    end
   end
 
   def create_listing(data)
-    response = post('/crm/sales/api/cpq/products', data)
-    response.success?
-  rescue ApiError => e
-    Rails.logger.error "Failed to create listing in Freshsales: #{e.message}"
-    false
+    handle_api_errors do
+      response = post('/crm/sales/api/cpq/products', data)
+      response.success?
+    end
   end
 end
