8ca87e17fbb15d542bf437e7409c000bfd316c05
Brian Murithi <brianmurithi65@gmail.com>
2024-09-23 12:55:20 +0300

    Rebase project

--- app/models/legacy/listing.rb
+++ app/models/legacy/listing.rb
@@ -45,5 +45,7 @@ module Legacy
     belongs_to :listing_location, class_name: 'Legacy::ListingLocation', foreign_key: 'location_id'
     belongs_to :listing_relationship_manager, class_name: 'Legacy::ListingRelationshipManager', foreign_key: 'seller_relation_manager_id'
     belongs_to :vehicle, class_name: 'Legacy::Vehicle', foreign_key: 'vehicle_id'
+    belongs_to :cover_photo, class_name: 'Legacy::ListingCoverPhoto', foreign_key: 'cover_image_id'
+
   end
 end
