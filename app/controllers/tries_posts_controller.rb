5861f12882d1df69e881e55433e91812bc2323c9
Zillah Mwimali <zillahmwimmali@gmail.com>
2024-10-09 15:25:06 +0300

    consuming ghost api to show posts

--- app/controllers/tries_posts_controller.rb
+++ app/controllers/tries_posts_controller.rb
@@ -1,13 +1,17 @@
 class TriesPostsController < ApplicationController
     def index
-        ghost_client = GhostClientService.new
-        @posts = ghost_client.fetch_posts
+      ghost_client = GhostClientService.new
+      response = ghost_client.fetch_posts
+  
+      if response.present?
+        @posts_data = response.body
 
-        if @posts.present?
-            render json: @posts.body
-        else
-            render json: { error: 'No posts found or error occurred' }, status: :unprocessable_entity
-        end
+    else
+        @posts_data = []
+      end
     end
-    
-end
+
+  end
+  
+  
+  
