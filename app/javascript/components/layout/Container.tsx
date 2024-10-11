8c70977170c3800adf89023cee6d79382caf003b
Brian Murithi <brianmurithi65@gmail.com>
2024-10-11 15:41:27 +0300

    Create component for constructing layouts

---
+++ app/javascript/components/layout/Container.tsx
@@ -0,0 +1,19 @@
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+  size?: 'full' | 'centered';
+}
+
+export default function Container({
+  children,
+  className = '',
+  size = 'full',
+}: ContainerProps) {
+  const containerClass = size === 'full' ? 'w-full' : 'max-w-7xl mx-auto';
+
+  return (
+    <div className={`${containerClass} ${className} `}>
+      <div>{children}</div>
+    </div>
+  );
+}
---
+++ app/javascript/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import Container from './Container';
+
+export default function Footer() {
+  return (
+    <Container size="centered">
+      <footer>
+        <div className="bg-pinkDark min-h-[11.9rem] rounded-t-lg"></div>
+        <div className="bg-pinkLight min-h-[40rem]"></div>
+      </footer>
+    </Container>
+  );
+}
